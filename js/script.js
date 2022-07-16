e6bd48ead36f55b9f0f8200f02915a3079bcf041
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-07-16 11:50:24 -0600

    Added my project

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) 
+mainnav.classList.remove("responsive");
+};
+
+
+function changewidth() { 
+  var x = document.getElementById('iframe'); 
+  x.style.width = "100%"; 
+}
+
+
